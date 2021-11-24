dca2430252606680c8da5caa31a444711ce2606d
bek <qwerty@gmail.com>
2021-11-24 21:11:48 +0500

    povtor

---
+++ script.js
@@ -0,0 +1,22 @@
+var str = 'приветствую';
+alert(str);
+var name = prompt('введите свое имя');
+var age = prompt('введите свой возраст');
+var about = 'отлично,приступим к примерам';
+alert(about);
+var plus = prompt(name + ' cколько будет ' + ' 6 + 6 ' );
+var minus = prompt( ' a ' + '7 - 7' );
+var multi = prompt(' a ' + ' 8 * 8 ');
+var divis = prompt(' a ' + '9 / 9 ');
+var remDevis = prompt(' a ' + ' 10 % 3' + ' - деление с отстатком ' );
+var theEnd = 'отлично, ответы можно посмотреть в консоли разработчика';
+alert(theEnd);
+
+
+console.log(' 6 + 6 = ' + ( 6 + 6 ) +  ' ( вы ответили ' + plus + ' ) ' );
+console.log(' 7 + 7 = ' + ( 7 + 7 ) + '( вы ответили ' + minus + ')' );
+console.log(' 8 * 8 = ' + ( 8 * 8 ) + '(вы ответили ' + multi + ')');
+console.log(' 9 / 9 = ' + ( 9 / 9 ) + '(вы ответили ' + divis + ')');
+console.log(' 10 % 3 = ' + ( 10 % 3 ) + '(вы ответили ' + remDevis + ')');
+
+
